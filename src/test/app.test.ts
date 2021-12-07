95cb1b16aeea0fb6e35363bead32b3904b2aae12
twaldorf <trevor.automated@gmail.com>
2021-12-07 12:38:42 -0800

    init csv export tests

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -3,11 +3,21 @@ import { agent as request } from 'supertest'
 
 describe("GET /", () => {
     test("api index request", async () => {
-      const response = await request(app).get("/");
-      expect(response.body.response).toEqual("Index");
-      expect(response.statusCode).toEqual(200);
+      const response = await request(app).get("/")
+      expect(response.body.response).toEqual("Index")
+      expect(response.statusCode).toEqual(200)
     })
-    afterAll(async () => {
-        server.close()
-    });
-  })
+})
+
+describe("Export CSV", () => {
+    test("csv route request", async () => {
+        const response = await request(app).get("/csv")
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.response.includes('CSV Generated')).toEqual(true)
+        expect(response.body.fileUrl ? true : false).toEqual(true)
+    })
+})
+
+afterAll(async () => {
+    server.close()
+})
